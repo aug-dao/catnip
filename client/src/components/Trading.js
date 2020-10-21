ea2cc6aaf819f84df92b3b8563fde8e0091eb71d
BenD <bendvw@gmail.com>
2020-10-20 17:05:26 -0700

    https for info link

--- client/src/components/Trading.js
+++ client/src/components/Trading.js
@@ -391,7 +391,7 @@ export default function Trading(props) {
               </div>
               <div>
                 <a
-                  href="http://www.predictionexplorer.com/market/0x1EBb89156091EB0d59603C18379C03A5c84D7355"
+                  href="https://www.predictionexplorer.com/market/0x1EBb89156091EB0d59603C18379C03A5c84D7355"
                   target="_blank"
                 >
                   {/* <img className={classes.info_icon} src={infoIcon} alt="info icon"/> */}
